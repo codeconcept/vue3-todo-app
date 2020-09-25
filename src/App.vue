048a20f6929401311a812285875c9da08163532f
codeconcept <contact@codeconcept.fr>
2020-09-25 10:34:41 +0200

    Form component with reactive variable

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Veille techno</h1>
+  <Form />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Form from "@/components/Form";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Form,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Form.vue
@@ -0,0 +1,30 @@
+<template>
+  <form @submit.prevent="addTechno">
+    <label for="techno">Techno</label>
+    <br />
+    <input type="text" name="techno" v-model="techToLearn" />
+    <br />
+    <button type="submit">ajouter</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    let techToLearn = ref("");
+    const addTechno = function () {
+      console.log("techToLearn", techToLearn.value);
+      techToLearn.value = "";
+    };
+
+    return {
+      techToLearn,
+      addTechno,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
